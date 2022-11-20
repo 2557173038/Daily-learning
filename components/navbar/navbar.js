946f7c478db11ae4f8f3ea1def9bad7ba0b4e14a
小企鹅 <2557173038@qq.com>
2022-11-20 16:55:27 +0800

    小程序22-自定义组件库

---
+++ components/navbar/navbar.js
@@ -0,0 +1,30 @@
+// components/nacbar/navbar.js
+// 注册一个组件
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    datalist:["正在热映","即将上映"],
+    id:''
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    handleClick(evt){
+      // console.log(evt.currentTarget.dataset.index)
+      this.setData({
+        id:evt.currentTarget.dataset.index
+      })
+    },
+  }
+})
